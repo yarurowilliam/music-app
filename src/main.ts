46396bf61951afb704e9a5bb91d38738c8fec354
yarurowilliam <williamyaruro1@gmail.com>
2025-03-02 16:02:10 -0500

    General: Ajustes en la inicialización de la aplicación

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FollowService } from './app/core/services/follow.service';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    importProvidersFrom(BrowserModule),
+    FollowService
   ]
 }).catch(err => console.error(err));
