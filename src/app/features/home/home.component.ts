74993160b34c2cf9b23bef8e6c8a1fe81d7ea10c
yarurowilliam <williamyaruro1@gmail.com>
2025-03-02 16:08:03 -0500

    Home: Ajustes en la página principal

---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<div>Home Component</div>`,
+  styles: []
+})
+export class HomeComponent {}
